ea59c30e871d0c0127a40848d2b783bd4a2e919f
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-05-06 21:20:31 +0100

    add comment

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -201,7 +201,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60638" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61347" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
